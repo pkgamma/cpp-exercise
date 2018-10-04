b57ccdcdffd8285778aa30bf0fcc32944959d393
pkgamma <pkgamma@gmail.com>
2018-10-03 22:44:04 -0500

    removed unnecessary comments

--- main.cpp
+++ main.cpp
@@ -1,16 +1,8 @@
-// Simple C++ program to display "Hello World" 
-
-// Header file for input output functions 
 #include<iostream> 
-
 using namespace std; 
 
-// main function - 
-// where the execution of program begins 
 int main() 
-{ 
-	// prints hello world 
+{
 	cout<<"Hello, CS126! Getting started in C++, this is exciting!\n"; 
-	
 	return 0; 
 } 
