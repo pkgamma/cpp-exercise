04b8ab9c86ccec93e3980e0e91cda99966739501
pkgamma <pkgamma@gmail.com>
2018-10-05 11:21:15 -0500

    Following Google's getting started guide to make some fun stuff!

--- main.cpp
+++ main.cpp
@@ -1,8 +1,15 @@
-#include<iostream> 
-using namespace std; 
+// hello.cpp: Philip Kuo
+// Description: c++ program, following what Google is telling me to do
 
-int main() 
-{
-	cout<<"Hello, CS126! Getting started in C++, this is exciting!\n"; 
-	return 0; 
-} 
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int i = 0; i < 4; i++) {
+        cout << "Hello, CS126! Getting started in C++, this is exciting!" << endl;
+    }
+
+    cout << "Editing in VSCode. Editing in Xcode now! Nope, in CLion now!" << endl;
+    return 0;
+}
