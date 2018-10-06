ca2b4762bcac7b3225da96635437a107a745e237
pkgamma <pkgamma@gmail.com>
2018-10-06 01:03:52 -0500

    Made some changes based on what the moderator told me in code review today.

--- main.cpp
+++ main.cpp
@@ -2,14 +2,37 @@
 // Description: c++ program, following what Google is telling me to do
 
 #include <iostream>
-using namespace std;
+using std::cout;
+using std::endl;
+using std::cin;
 
 int main() {
 
-    for (int i = 0; i < 4; i++) {
+    const int MAX_LOOPS = 5;
+
+    for (int i = 0; i < MAX_LOOPS; i++) {
         cout << "Hello, CS126! Getting started in C++, this is exciting!" << endl;
     }
 
-    cout << "Editing in VSCode. Editing in Xcode now! Nope, in CLion now!" << endl;
+    cout << "Editing in VSCode. Editing in Xcode now! Nope, in CLion now! Should I try Vim?" << endl;
+
+    int input_var = 0;
+
+    do {
+        cout << "Enter a number, or enter -1 to quit: ";
+
+        if (!(cin >> input_var)) {
+            cout << "NOT NUMBER!" << endl;
+            break;
+        }
+
+        if (input_var != -1) {
+            cout << "You entered: " << input_var << endl;
+        }
+
+    } while (input_var != -1);
+
+    cout << "Bye bye." << endl;
+
     return 0;
 }
