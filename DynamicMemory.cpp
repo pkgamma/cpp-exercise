e24c138d4836c40adc4e82d0aff8b779f3a7d71e
pkgamma <pkgamma@gmail.com>
2018-10-07 22:58:26 -0500

    Dangling Pointers are cool.

--- DynamicMemory.cpp
+++ DynamicMemory.cpp
@@ -1,4 +1,31 @@
 #include <iostream>
 using namespace std;
 
+// Many times, you are not aware in advance how much memory you will need to
+// store particular information in a defined variable and the size of required
+// memory can be determined at run time.
 
+// You can allocate memory at run time within the heap for the variable of a
+// given type using the new operator, which returns the address of the space
+// allocated.
+
+int main() {
+    new int;
+
+    //! The pointer p is stored in the stack as a local variable, and holds the
+    //! heap's allocated address as its value. The value of 5 is stored at that
+    //! address in the heap.
+    int *p = new int;
+
+    *p = 5;
+
+    cout << *p << endl;
+
+    delete p;  // free up the memory
+
+    //! The delete operator frees up the memory allocated for the variable, but
+    //! does not delete the pointer itself, as the pointer is stored on the
+    //! stack.
+
+    p = new int;  // reuse for a new address
+}
