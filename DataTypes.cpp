325f3079ba79c2cf91b941dba1b8a50ef5603790
pkgamma <pkgamma@gmail.com>
2018-10-07 21:37:04 -0500

    Made changes to data types cpp file.

--- DataTypes.cpp
+++ DataTypes.cpp
@@ -1,4 +1,13 @@
 #include <iostream>
+
+// gotta include <string> to use string, unlike in Java,
+// this time you actally have to work for it.
+
+// BUT BRO! The <string> library is included in the <iostream> library, so you
+// don't need to include <string> separately, if you already use <iostream>.
+
+// ! C++ is case-sensitive (better comment plugin made this red.)
+
 using namespace std;
 
 // signed: A signed integer can hold both negative and positive numbers.
@@ -7,10 +16,25 @@ using namespace std;
 // long: Twice the default size.
 
 int main() {
-    unsigned long int a = 42; // positive num only
+    unsigned long int a = 42;  // positive num only
 
-    float b = 4.20; // decimal points ok
+    float b = 4.20;  // decimal points ok
 
+    string name = "Philip Kuo";
+
+    bool tf = true;
+
+    if (tf) {
+        cout << "The bool value is true." << endl;
+    }
+
+    cout << name << endl;
     cout << b << endl;
     cout << a << endl;
+
+    // ARRAYS!
+
+    int arr[] = {0, 1, 2, 3, 4};
+
+    cout << arr[3] << endl;
 }
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # C++ Hello World Program 
-Getting Started with C++, all the mini programs here are from online sources, modified by Philip Kuo.
+Getting Started with C++, all the mini programs here are from online sources (most of them are from SoloLearn), modified by Philip Kuo.
